e8b4ef4d2f757fc33c9362fec2dbf4168a9209ca
yewei600 <ericwei94@gmail.com>
2016-07-16 17:03:02 -0400

    server port num

--- runserverfg.sh
+++ runserverfg.sh
@@ -2,5 +2,5 @@
 
 export CLASSPATH=".:build:lib/*"
 
-java ece454.StorageNode localhost 5567 ecelinux3.uwaterloo.ca:2815 /$USER
+java ece454.StorageNode localhost 2913 ecelinux3.uwaterloo.ca:2815 /$USER
 
